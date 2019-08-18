3c285cb6b7053f58a5624929416b978391fa37f7
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-08-18 17:38:16 +0300

    Update index.jsx

--- client/src/components/FormValidator/index.jsx
+++ client/src/components/FormValidator/index.jsx
@@ -10,17 +10,15 @@ class FormValidator {
     let validation = this.valid();
     this.validations.forEach(rule => {
       if (!validation[rule.field].isInvalid) {
+
         const field_value = state[rule.field].toString();
         const args = rule.args || [];
-        const validation_method =
-              typeof rule.method === 'string' ?
-              validator[rule.method] :
-              rule.method
+        const validation_method = validator[rule.method]
 
         if(validation_method(field_value, ...args, state) !== rule.validWhen) {
           validation[rule.field] = { isInvalid: true, message: rule.message }
           validation.isValid = false;
-        }
+          }
       }
     });
     return validation;
