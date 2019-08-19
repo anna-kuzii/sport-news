5b38d93b7793ec70b01eaac50357dcea58e04b3c
forenZZy <37437522+forenZZy@users.noreply.github.com>
2019-08-19 19:38:13 +0300

    Update register_controller.js

--- controllers/register_controller.js
+++ controllers/register_controller.js
@@ -8,7 +8,7 @@ const encrypt = require('./encrypt');
 exports.registerController = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+    return res.status(422).json({ message: 'Invalid input' });
   }
   const { password, email } = req.body;
   return UserQuery.findUserOne(email)
