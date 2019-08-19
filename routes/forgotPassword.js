9d50e5d5215b0921f31eb44d9afb8cd59a943fc8
AndriyRubanskui <rubansku97@gmail.com>
2019-08-19 17:59:18 +0300

    add TODO

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
+  // TODO render a forgotPassword form
   res.send('it is a forgot password block');
 });
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
+  // TODO: connect a login form
   res.send('it is a login block');
 });
 module.exports = router;
