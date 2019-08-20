e0d1ac146c5cd280331dab21f6c68542cc871a3d
roman454647 <roman.pidkostelnii@gmail.com>
2019-08-20 16:26:01 +0300

    fixed eslint bugs

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import './index.scss'
 
-import { store } from './_helpers';
-import App from './App';
+import { store } from './_helpers'
+import App from './App'
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'));
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
