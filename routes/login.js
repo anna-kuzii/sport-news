0558641a1dd682e0c07970053e8544275c7c76b1
forenZZy <37437522+forenZZy@users.noreply.github.com>
2019-08-22 01:51:08 +0300

    be auth

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 router.get('/', (req, res) => {
   // TODO: connect a login form
   res.send('it is a login block');
 });
+
+router.post('/',authController.authorization);
 module.exports = router;
