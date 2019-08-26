9870009d22aeb9320a251c092db82eed3e783c65
AndriyRubanskui <rubansku97@gmail.com>
2019-08-26 17:38:12 +0300

    fixed

--- client/src/components/Share/index.jsx
+++ client/src/components/Share/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './style.scss';
 
-const Share = () => (
+export const Share = () => (
         <div className='services'>
           <span>Our Social Networks</span>
           <div>
-          <a href="https://www.facebook.com/" target='blank' className='facebook-icon'></a>
-          <a href="https://twitter.com/" target='blank' className='twitter-icon'></a>
-          <a href="https://www.linkedin.com/" target='blank' className='linkedin-icon'></a>
+          <a href="https://www.facebook.com/" target='_blank' className='facebook-icon icon'></a>
+          <a href="https://twitter.com/" target='_blank' className='twitter-icon icon'></a>
+          <a href="https://www.linkedin.com/" target='_blank' className='linkedin-icon icon'></a>
           </div>
         </div>
   );
 
-  export default Share; 
