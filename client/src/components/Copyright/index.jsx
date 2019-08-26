af20797714d78bb8674a367c0778e22123519da9
AndriyRubanskui <rubansku97@gmail.com>
2019-08-26 15:05:04 +0300

    copyright

---
+++ client/src/components/Copyright/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+const Copyright = () => (
+        <div className='cop-container'>
+          <h2>Sport News</h2>
+          <div>
+          <h4>Copyright ©2019 Sport News</h4>
+          <h4 className='privacy'>Privacy/Terms</h4>
+          </div>
+        </div>
+  );
+  
+  export default Copyright;
