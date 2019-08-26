41d641571570bbb312d1bbb52afb200046e06cdf
AndriyRubanskui <rubansku97@gmail.com>
2019-08-26 15:57:03 +0300

    share-links-02

---
+++ client/src/components/Share/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+const Share = () => (
+        <div className='services'>
+          <span>Our Social Networks</span>
+          <div>
+          <a href="https://www.facebook.com/" target='blank' className='facebook-icon'></a>
+          <a href="https://twitter.com/" target='blank' className='twitter-icon'></a>
+          <a href="https://www.linkedin.com/" target='blank' className='linkedin-icon'></a>
+          </div>
+        </div>
+  );
+
+  export default Share; 
