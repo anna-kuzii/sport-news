9c30d3831612315a124ab00dde112320dfa37991
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-08-28 09:38:49 +0300

    removed hr and fixed some bugs

--- client/src/components/NewsOfTheDays/index.jsx
+++ client/src/components/NewsOfTheDays/index.jsx
@@ -1,29 +1,26 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import './style.scss'
 import { Link } from 'react-router-dom'
 
 export const NewsOfTheDays = (props) => {
   const { image, tittle, text, photo_courtesy, full_news } = props
-
   function showSeeMoreButton(text, full_news) {
-    return text.length <60 ? text
-      : <Fragment>{text.slice(0, 60)}<Link to={full_news} className='see-more'> ...</Link></Fragment>
+    return text.length <100 ? text
+      : <p>{text.slice(0, 60)}<Link to={full_news} className='see-more'> ...</Link></p>
   }
 
   return (
-    <div className='newsoftheday-wrapper'>
+    <div className='newsofthedayWrapper'>
       <div className='newsDivider'>
-        <hr />
         <p>photo of the day</p>
-        <hr />
       </div>
       <div className='newsBlock'>
         <img src={image} alt='background' />
-        <div className='photoOftheDay-triangle'>
+        <div className='photoOftheDayTriangle'>
           <p>photo<span><br />of the<br /></span>day</p>
         </div>
         <div className='textOfNews'>
-          <h1 className='tittle'>{tittle}</h1>
+          <h1 className='title'>{tittle}</h1>
           <p className='newsText'>{showSeeMoreButton(text, full_news)}</p>
           <div className='photoCourtesy'>{photo_courtesy}</div>
         </div>
