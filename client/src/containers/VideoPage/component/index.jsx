5bd8718c2e569a8cfb9d89cc836e497dd362b7ca
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-09-09 19:26:17 +0300

    Update index.jsx

--- client/src/containers/VideoPage/component/index.jsx
+++ client/src/containers/VideoPage/component/index.jsx
@@ -14,79 +14,77 @@ class VideoPage extends Component {
   render() {
     const { loading, videoData } = this.props;
 
-    return (
-      !videoData.length && loading
-        ?(
-          <div className='data-loading'>
-            <Loader
-              type='TailSpin'
-              color='#C63638'
-              height='200'
-              width='200'
-            />
+    if (!videoData.length) {
+      return loading ? (
+        <div className='data-loading'>
+          <Loader
+            type='TailSpin'
+            color='#C63638'
+            height='200'
+            width='200'
+          />
+        </div>
+      )
+        : (
+          <div className='emptyDB'>
+              sorry we don`t have any video :-)
           </div>
-        )
-        : (!loading && !videoData.length)
-          ? (
-            <div className='emptyDB'>
-            sorry we don`t have any video :-)
-            </div>
-          )
-          : (
-            <div className='video-page-wrapper'>
-              <div className='news-word-wrap'>
+        );
+    }
+    return (
+      <div className='video-page-wrapper'>
+        <div className='news-word-wrap'>
                   NEWS
-              </div>
-              <div className='video-word-wrap'>
+        </div>
+        <div className='video-word-wrap'>
                    VIDEO
-              </div>
-              <div className='section-name'>
-                <p><span>video</span> &gt; the NBA videos</p>
-              </div>
-              <div className='main-video-wrapper'>
-                <iframe
-                  className='main-video'
-                  title={videoData[0].title}
-                  src={videoData[0].videoURL}
-                />
-                <div className='main-title' >{videoData[0].title}</div>
-              </div>
-              <div className='additional-video-wrapper'>
-                <div className='left-column-video'>
-                  <div className='single-video'>
-                    <iframe
-                      title={videoData[1].title}
-                      src={videoData[1].videoURL}
-                    />
-                    <h1>{videoData[1].title}</h1>
-                  </div>
-                  <div className='single-video'>
-                    <iframe
-                      title={videoData[2].title}
-                      src={videoData[2].videoURL}
-                    />
-                    <h1>{videoData[2].title}</h1>
-                  </div>
-                </div>
-                <div className='right-column-video'>
-                  <div className='single-video'>
-                    <iframe
-                      title={videoData[3].title}
-                      src={videoData[3].videoURL}
-                    />
-                    <h1>{videoData[3].title}</h1>
-                  </div>
-                  <div className='single-video'>
-                    <iframe
-                      title={videoData[4].title}
-                      src={videoData[4].videoURL}
-                    />
-                    <h1>{videoData[4].title}</h1>
-                  </div>
-                </div>
-              </div>
+        </div>
+        <div className='section-name'>
+          <p><span>video</span> &gt; the NBA videos</p>
+        </div>
+        <div className='main-video-wrapper'>
+          <iframe
+            className='main-video'
+            title={videoData[0].title}
+            src={videoData[0].videoURL}
+          />
+          <div className='main-title' >{videoData[0].title}</div>
+        </div>
+        <div className='additional-video-wrapper'>
+          <div className='left-column-video'>
+            <div className='single-video'>
+              <iframe
+                title={videoData[1].title}
+                src={videoData[1].videoURL}
+              />
+              <h1>{videoData[1].title}</h1>
             </div>
-          )
+            <div className='single-video'>
+              <iframe
+                title={videoData[2].title}
+                src={videoData[2].videoURL}
+              />
+              <h1>{videoData[2].title}</h1>
+            </div>
+          </div>
+          <div className='right-column-video'>
+            <div className='single-video'>
+              <iframe
+                title={videoData[3].title}
+                src={videoData[3].videoURL}
+              />
+              <h1>{videoData[3].title}</h1>
+            </div>
+            <div className='single-video'>
+              <iframe
+                title={videoData[4].title}
+                src={videoData[4].videoURL}
+              />
+              <h1>{videoData[4].title}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 };
