eee40a48890996a25b203e74421aefa929f6c311
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-09-15 19:23:48 +0300

    changed interceptors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 import './App.scss';
 import { Routes } from './Routes';
-import { authInterceptor } from './axios.instanse';
-
-authInterceptor();
 
 const App = () => (
   <Routes />
--- client/src/axios.instanse.js
+++ client/src/axios.instanse.js
@@ -1,24 +1,22 @@
 import axios from 'axios';
 import { logoutUser } from './containers/Logout/action';
+import { store, history } from './helpers';
 
 require('dotenv').config();
-
-export const authInterceptor = (store, history) => {
-  axios.interceptors.response.use((response) => {
-    return response;
-  }, (error) => {
-    if (error.response.status === 401) {
-      store.dispatch(logoutUser());
-      localStorage.removeToken('accessToken');
-      history.push('/login');
-    }
-    return Promise.reject(error);
-  });
-};
-
 
 export var instance = axios.create(
   {
     baseURL: process.env.REACT_APP_SERVER_URL,
   }
 );
+
+instance.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  if (error.response.status === 401) {
+    store.dispatch(logoutUser());
+    localStorage.removeToken('accessToken');
+    history.push('/login');
+  }
+  return Promise.reject(error);
+});
